8a419ded9a7e5795aed17b7a1fd292870d17bf60
Ritika Marotha <ritikamarotha9@gmail.com>
2025-05-12 16:45:28 +0530

    Add files via upload

---
+++ portfolio ritika/main.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,  
+    backDelay: 1000,})   
+
+    fetch('/your-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: 'John',
+          email: 'john@example.com'
+        })
+      });
+      
